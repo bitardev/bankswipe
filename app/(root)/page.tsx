438c9d7d0ee99109f395bead861ac7ec1226c103
Bitar Youssef <bitardev@gmail.com>
2024-10-30 09:56:38 +0100

    implement right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Youssef" };
+  const loggedIn = {
+    firstName: "Youssef",
+    lastName: "Bitar",
+    email: "bitardev@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -14,13 +19,20 @@ const Home = () => {
             user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
+
+          <TotalBalanceBox
+            accoounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
         </header>
-        <TotalBalanceBox
-          accoounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250.35}
-        />
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
     </section>
   );
 };
