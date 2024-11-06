61f45fb3f585cb471880be74e4f5477596fd63be
Bitar Youssef <bitardev@gmail.com>
2024-11-06 07:42:51 +0100

    Setup Sentry to monitor the user workflow

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c1d6f0094f459535197333a6365c49f4@o4508249930268672.ingest.de.sentry.io/4508249933807696",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
